b8429ae630376a2137b62e97b6bed1057667ddf3
SHU <shu030929@gmail.com>
2025-08-26 17:44:48 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 9.39 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -0,0 +1,12 @@
+def solution(a, b):
+    answer = 0
+    temp1 = 0
+    temp2 = 0
+    
+    temp1 = str(a)+str(b)
+    temp2 = str(b)+str(a)
+    
+    
+    
+    
+    return int(max(temp1,temp2))
