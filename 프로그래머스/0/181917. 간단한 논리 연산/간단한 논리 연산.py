0bbbc5875afc0add54a7d307884c6325916d7ed5
SHU <shu030929@gmail.com>
2025-09-02 17:41:25 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.00 ms, Memory: 9.32 MB -BaekjoonHub

---
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.py
@@ -0,0 +1,12 @@
+def solution(x1, x2, x3, x4):
+
+    
+    if (x1 or x2) and (x3 or x4) is True:
+        answer = True
+    else:
+        answer = False
+    
+    
+    
+    
+    return answer
