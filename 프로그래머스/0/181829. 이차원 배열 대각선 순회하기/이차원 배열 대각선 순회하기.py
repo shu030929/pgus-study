036396b70d2cb6a5c060f279f9e1aa600895f162
SHU <shu030929@gmail.com>
2025-09-14 02:25:14 +0900

    [level 0] Title: 이차원 배열 대각선 순회하기, Time: 0.21 ms, Memory: 9.34 MB -BaekjoonHub

---
+++ 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.py
@@ -0,0 +1,14 @@
+def solution(board, k):
+    answer = 0
+    
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            
+            
+            
+            if i+j <= k:
+                answer += board[i][j]
+            else:
+                answer += 0
+    
+    return answer
