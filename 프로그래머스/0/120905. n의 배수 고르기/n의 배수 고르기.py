be41a7f8fb8e00be26b2f142b76d941b8657f04a
SHU <shu030929@gmail.com>
2025-09-18 20:16:01 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.01 ms, Memory: 9.27 MB -BaekjoonHub

---
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.py
@@ -0,0 +1,10 @@
+def solution(n, numlist):
+    answer = []
+    
+    for i in range(len(numlist)):
+        if int(numlist[i])%n == 0:
+            answer.append(numlist[i])
+    
+    
+    
+    return answer
